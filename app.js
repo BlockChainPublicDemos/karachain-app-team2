80a5e82d5e475b09dcc0a85f4c58fe85ef3bcb4f
Stuart Bedoll <bedoll@us.ibm.com>
2017-03-01 19:17:07 -0700

    try marbles

--- app.js
+++ app.js
@@ -230,12 +230,12 @@ var options = 	{
 //						unzip_dir: 'marbles/chaincode',	
 //						git_url: 'http://gopkg.in/ibm-blockchain/marbles.v2/chaincode'								//subdirectroy name of chaincode after unzipped
 //						git_url: 'https://github.com/ibm-blockchain/marbles/chaincode'					//GO get http url
-//						zip_url: 'https://github.com/BlockChainPublicDemos/karachain-app-team2/archive/master.zip',		//karachain repo archive
-//						unzip_dir: 'karachain-app-team2-master/chaincode',													//subdirectroy name of chaincode after unzipped
-//						git_url: 'https://github.com/BlockChainPublicDemos/karachain-app-team2/chaincode'
-							zip_url: 'https://github.com/sbedoll/chaincode-marbles/archive/master.zip',		//karachain repo archive
-							unzip_dir: 'chaincode-marbles-master/chaincode',													//subdirectroy name of chaincode after unzipped
-							git_url: 'https://github.com/sbedoll/chaincode-marbles/chaincode'//karachain repo
+						zip_url: 'https://github.com/BlockChainPublicDemos/karachain-app-team2/archive/master.zip',		//karachain repo archive
+						unzip_dir: 'karachain-app-team2-master/marblescc',													//subdirectroy name of chaincode after unzipped
+						git_url: 'https://github.com/BlockChainPublicDemos/karachain-app-team2/marblescc'
+//							zip_url: 'https://github.com/sbedoll/chaincode-marbles/archive/master.zip',		//karachain repo archive
+//							unzip_dir: 'chaincode-marbles-master/chaincode',													//subdirectroy name of chaincode after unzipped
+//							git_url: 'https://github.com/sbedoll/chaincode-marbles/chaincode'//karachain repo
 					
 						//hashed cc name from prev deployment, comment me out to always deploy, uncomment me when its already deployed to skip deploying again
 						//deployed_name: '16e655c0fce6a9882896d3d6d11f7dcd4f45027fd4764004440ff1e61340910a9d67685c4bb723272a497f3cf428e6cf6b009618612220e1471e03b6c0aa76cb'
