63d1bec914cd6d9eedef801878ac103c2a04e302
liguda <liguda@IBM481-PBD8EKD>
2017-03-02 13:44:19 +0100

    Fixing error in invoke for structure...I guess

--- chaincode/KaraChainV2.go
+++ chaincode/KaraChainV2.go
@@ -75,7 +75,7 @@ type Song struct {
 	Venue_Name                 string `json:"Venue_Name"`
 	User_Id                    string `json:"User_Id"`
 	User_role                  string `json:"User_role"`
-	User_rating                string `json:"User_role"`
+	User_rating                string `json:"User_rating"`
 	Obsolete                   bool   `json:"Obsolete"`
 	Status                     string `json:"Status"`
 }
@@ -431,34 +431,35 @@ func (t *SimpleChaincode) create_song(stub shim.ChaincodeStubInterface, caller s
 	var s Song
 
 	Song_ID := "\"Song_ID\":\"" + Song_ID_r + "\", " // Variables to define the JSON
-	//	Date_created := "\"Date_created\":\"UNDEFINED\", "
-	//	SmartContract_Unique_ID := "\"SmartContract_Unique_ID\":0, "
-	//	Singer_Id := "\"Singer_Id\":\"UNDEFINED\", "
-	//	Singer_Name := "\"Singer_Name\":\"UNDEFINED\", "
-	//	Video_Id := "\"Video_Id \":\"UNDEFINED\", "
-	//	owner := "\"Owner\":\"" + caller + "\", "
-	//	Video_Link := "\"Video_Link\":\"UNDEFINED\", "
-	//	Video_date_created := "\"Video_date_created\":\"UNDEFINED\", "
-	//	Video_QR_code_Id := "\"Video_QR_code_Id\":\"UNDEFINED\", "
-	//	Copyright_Id := "\"Copyright_Id\":\"UNDEFINED\""
-	//	Copyright_date_created := "\"Copyright_date_created\":\"UNDEFINED\", "
-	//	Copyright_date_accepted := "\"Copyright_date_accepted\":\"UNDEFINED\", "
-	//	Copyright_date_rejected := "\"Copyright_date_rejected\":\"UNDEFINED\", "
-	//	Copyright_Institution_Id := "\"Copyright_Institution_Id\":\"UNDEFINED\", "
-	//	Copyright_Institution_Name := "\"Copyright_Institution_Name\":\"UNDEFINED\", "
-	//	Copyright_State := "\"Copyright_State\":\"UNDEFINED\", "
-	//	Venue_Id := "\"Venue_Id\":\"UNDEFINED\", "
-	//	Venue_Name := "\"Venue_Name\":\"UNDEFINED\", "
-	//	User_Id := "\"User_Id\":\"UNDEFINED\", "
-	//	User_role := "\"User_role\":\"UNDEFINED\", "
+	Date_created := "\"Date_created\":\"UNDEFINED\", "
+	SmartContract_Unique_ID := "\"SmartContract_Unique_ID\":0, "
+	Singer_Id := "\"Singer_Id\":\"UNDEFINED\", "
+	Singer_Name := "\"Singer_Name\":\"UNDEFINED\", "
+	Video_Id := "\"Video_Id\":\"UNDEFINED\", "
+	Owner := "\"Owner\":\"UNDEFINED\", "
+	Video_Link := "\"Video_Link\":\"UNDEFINED\", "
+	Video_date_created := "\"Video_date_created\":\"UNDEFINED\", "
+	Video_QR_code_Id := "\"Video_QR_code_Id\":\"UNDEFINED\", "
+	Copyright_Id := "\"Copyright_Id\":\"UNDEFINED\""
+	Copyright_date_created := "\"Copyright_date_created\":\"UNDEFINED\", "
+	Copyright_date_accepted := "\"Copyright_date_accepted\":\"UNDEFINED\", "
+	Copyright_date_rejected := "\"Copyright_date_rejected\":\"UNDEFINED\", "
+	Copyright_Institution_Id := "\"Copyright_Institution_Id\":\"UNDEFINED\", "
+	Copyright_Institution_Name := "\"Copyright_Institution_Name\":\"UNDEFINED\", "
+	Copyright_State := "\"Copyright_State\":\"UNDEFINED\", "
+	Venue_Id := "\"Venue_Id\":\"UNDEFINED\", "
+	Venue_Name := "\"Venue_Name\":\"UNDEFINED\", "
+	User_Id := "\"User_Id\":\"UNDEFINED\", "
+	User_role := "\"User_role\":\"UNDEFINED\", "
+	User_rating := "\"User_rating\":\"UNDEFINED\", "
 	Obsolete := "\"Obsolete\":false, "
-	Status := "\"Status\":false"
+	Status := "\"Status\":\"UNDEFINED\""
 
-	//	Song_json := "{" + Song_ID + Date_created + SmartContract_Unique_ID + Singer_Id + Singer_Name + Video_Id + owner + Video_Link + Video_date_created + Video_QR_code_Id +
-	//		Copyright_Id + Copyright_date_created + Copyright_date_accepted + Copyright_date_rejected + Copyright_Institution_Id + Copyright_Institution_Name + Copyright_State +
-	//		Venue_Id + Venue_Name + User_Id + User_role + Obsolete + Status + "}" // Concatenates the variables to create the total JSON object
+	Song_json := "{" + Song_ID + Date_created + SmartContract_Unique_ID + Singer_Id + Singer_Name + Video_Id + Owner + Video_Link + Video_date_created + Video_QR_code_Id +
+		Copyright_Id + Copyright_date_created + Copyright_date_accepted + Copyright_date_rejected + Copyright_Institution_Id + Copyright_Institution_Name + Copyright_State +
+		Venue_Id + Venue_Name + User_Id + User_role + User_rating + Obsolete + Status + "}" // Concatenates the variables to create the total JSON object
 
-	Song_json := "{" + Song_ID + Obsolete + Status + "}" // Concatenates the variables to create the total JSON object
+	//	Song_json := "{" + Song_ID + Obsolete + Status + "}" // Concatenates the variables to create the total JSON object
 
 	// Do we need a certain criteria for a song ID?
 	_, err := regexp.Match("^[A-z][A-z][0-9]{7}", []byte(Song_ID)) // matched = true if the Song ID passed fits format of two letters followed by seven digits
