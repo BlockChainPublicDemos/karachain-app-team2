4320a37da978970e58c1c9caab3882c7a9021707
liguda <liguda@IBM481-PBD8EKD>
2017-03-14 22:49:35 +0100

    Update set_contract

--- chaincode/KaraChainV2.go
+++ chaincode/KaraChainV2.go
@@ -1213,7 +1213,7 @@ func main() {
 	err := shim.Start(new(SimpleChaincode))
 
 	if err != nil {
-		fmt.Printf("Error when starting Chaincode: %s", err)
+		fmt.Printf("Error starting Chaincode: %s", err)
 	}
 
 }
